f95cdf724f5e14fb6951048107bb333c82931e46
Margaret Lazarovits <mlazarovits@gmail.com>
2022-09-21 13:14:20 -0500

    updated dataprep class - changed sample scale factor from 156 to 1

--- python/DataPrep.py
+++ python/DataPrep.py
@@ -3,8 +3,8 @@ class DataPrep:
 	def __init__(self,dataset):
 		self.dataset = np.load(dataset, allow_pickle=True)
 		x, y = self.dataset['X'], self.dataset['y']
-		self.x_ns = x*156
-		self.y_ns = y*156
+		self.x_ns = x#*0.156
+		self.y_ns = y#*0.156
 		
 	def getSamples(self):
 		return self.x_ns, self.y_ns
